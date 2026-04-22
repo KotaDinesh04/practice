c071d6c8b9bda54eca154342c8fec13bae79b9c9
Dinesh Kota <mail.kotadinesh@gmail.com>
2026-04-22 09:40:01 +0530

    Connected to the backend

---
+++ frontend/src/Pages/Profile/ProfilePage.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import Service from '../../utils/http.js'
+
+const service = new Service();
+
+export const ProfilePage = () => {
+    return (
+        <div className='ProfilePage'>
+            <h1>hello</h1>
+        </div>
+    );
+};
+
+export default ProfilePage;
